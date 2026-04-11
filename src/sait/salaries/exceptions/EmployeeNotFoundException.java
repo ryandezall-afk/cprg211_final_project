a5e00b699b245877bcf3bb47b238e3f119f8fb89
jericacilo <eric.acilo@live.com>
2026-04-11 04:33:25 -0600

    init employe not found exception

--- src/sait/salaries/exceptions/EmployeeNotFoundException.java
+++ src/sait/salaries/exceptions/EmployeeNotFoundException.java
@@ -1,5 +1,24 @@
 package sait.salaries.exceptions;
 
-public class EmployeeNotFoundException {
+/**
+ * Class: EmployeeNotFoundException
+ * Author: Ryan Dezall, Jabin Unger, John Eric Acilo
+ * Date: April 11, 2026
+ *
+ * Description:
+ * This custom exception is used when the application attempts to find, update,
+ * or delete an employee record that does not exist in the system. Inputs for
+ * this exception are usually an employee ID or a search action that returns no
+ * matching result. The processing allows the application to stop the current
+ * operation safely and return a meaningful message to the user instead of
+ * crashing. The output is cleaner error handling for CLI and database-based
+ * operations throughout the project.
+ */
+public class EmployeeNotFoundException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
 }
